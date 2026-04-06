d5239ac2181c25b42ed1b5e7d2276e96acf824c8
ShakedGoffer <shakedgoffer@gmail.com>
2026-04-06 09:29:22 -0700

    fix: costumize X close btn

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -69,7 +69,7 @@ function DialogContent({
           <DialogPrimitive.Close data-slot="dialog-close" asChild>
             <Button
               variant="ghost"
-              className="absolute top-4 right-4"
+              className="absolute top-2 right-2 hover:bg-primary rounded-full p-1"
             >
               <XIcon
               />
