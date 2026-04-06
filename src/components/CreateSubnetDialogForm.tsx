97021d58e9e03258ce668f42542ad43f33c3c408
ShakedGoffer <shakedgoffer@gmail.com>
2026-04-06 09:41:23 -0700

    save changes

--- src/components/CreateSubnetDialogForm.tsx
+++ src/components/CreateSubnetDialogForm.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -12,10 +11,12 @@ import { Network } from "lucide-react";
 
 type FormSchemaType = {
   subnetName: string;
-  password: string;
+  subnetDescription: string;
+  subnetAddress: string;
+  subnetCIDR: string;
 };
 
-const CreateSubnetDialogForm = ({ title }) => {
+const CreateSubnetDialogForm = ({ title }: { title: string }) => {
   const form = useForm<FormSchemaType>();
 
   const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
@@ -32,7 +33,13 @@ const CreateSubnetDialogForm = ({ title }) => {
 
   return (
     <form className="flex flex-col gap-4">
-      <DialogContent className="w-md bg-form-bg">
+      {/* Dialog Form, dissable dialog close on outside interaction */}
+      <DialogContent
+        onInteractOutside={(e) => {
+          e.preventDefault();
+        }}
+        className="w-md bg-form-bg"
+      >
         <DialogHeader>
           <DialogTitle className="text-xl text-center capitalize font-bold">
             {title}
@@ -43,14 +50,26 @@ const CreateSubnetDialogForm = ({ title }) => {
           {...form.register("subnetName", { required: "Input is required" })}
           error={form.formState.errors.subnetName?.message}
           label="Subnet Name"
-          icon={<Network className="siz-4" />}
+          icon={<Network className="size-5" />}
         />
         <FormInput
-          {...form.register("password", { required: "Input is required" })}
-          error={form.formState.errors.password?.message}
-          label="Password"
-          type="password"
+          {...form.register("subnetDescription")}
+          error={form.formState.errors.subnetDescription?.message}
+          label="Subnet Description"
         />
+        <div className="flex flex-row items-center">
+          <FormInput
+            {...form.register("subnetAddress")}
+            error={form.formState.errors.subnetAddress?.message}
+            label="Subnet Address"
+          />
+          <span className="mx-3 text-secondary-text text-3xl ">/</span>
+          <FormInput
+            {...form.register("subnetCIDR")}
+            error={form.formState.errors.subnetCIDR?.message}
+            label="CIDR"
+          />
+        </div>
 
         <DialogFooter className="flex flex-col sm:flex-col gap-2">
           <Button
