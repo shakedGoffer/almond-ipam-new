eee74d9f1a58405f7f138792492b3dce3adf3bb7
ShakedGoffer <shakedgoffer@gmail.com>
2026-04-09 09:59:49 -0700

    fix: handle undefine/unknown subnet address

--- src/pages/SubnetDetailsPage.tsx
+++ src/pages/SubnetDetailsPage.tsx
@@ -33,10 +33,15 @@ import { Separator } from "@/components/ui/separator";
 import SubnetDialogForm from "@/components/dialogs/SubnetDialogForm";
 import DeleteConfirmationDialog from "@/components/dialogs/DeleteConfirmationDialog";
 import AddressDialogForm from "@/components/dialogs/AddressDialogForm";
+import { toast } from "sonner";
 
 const SubnetDetailsPage = () => {
   const { subnetAddress } = useParams();
-  const subnet: Subnet = fakeData[subnetAddress];
+  // Check subnetAddress key exists in data
+  const subnet: Subnet | undefined =
+    subnetAddress && subnetAddress in fakeData
+      ? fakeData[subnetAddress as keyof typeof fakeData]
+      : undefined;
 
   // Columns for Addresses DataTable
   const subnetAddressesColumns: ColumnDef<Ip>[] = [
@@ -105,108 +110,114 @@ const SubnetDetailsPage = () => {
     },
   ];
 
-  return (
-    <div className="flex flex-col flex-1 min-w-min gap-5">
-      {/* Top tools bar (delete & edit subnet + create new address + go back) */}
-      <div className="flex flex-row items-center justify-between text-primary-text">
-        <Link to={`/subnets`}>
-          <ChevronsLeft />
-        </Link>
-        <div className="flex flex-row gap-2">
-          <DropdownMenu>
-            <div className="flex flex-col w-full flex-1">
-              <DropdownMenuTrigger asChild className="">
-                <Button className="gap-1 px-10">
-                  <Plus />
-                  Add Address
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <AddressDialogForm
-                  variant="create"
-                  addressType="reserved"
-                  title={`Reserve Address`}
-                  description={`Reserve Address On Subnet: ${subnetAddress}/${subnet.subnet_cidr}`}
-                  dialogTrigger={
-                    <DropdownMenuItem className="text-address-reserved justify-center">
-                      Reserve Address
-                    </DropdownMenuItem>
-                  }
-                />
-                <DropdownMenuSeparator />
-                <AddressDialogForm
-                  variant="create"
-                  addressType="dynamic"
-                  title={`Allocate Free Address`}
-                  description={` Allocate Free Address On Subnet: ${subnetAddress}/${subnet.subnet_cidr}`}
-                  dialogTrigger={
-                    <DropdownMenuItem className="text-address-dynamic justify-center">
-                      Allocate Free Address
-                    </DropdownMenuItem>
-                  }
-                />
-              </DropdownMenuContent>
-            </div>
-          </DropdownMenu>
+  if (subnet) {
+    return (
+      <div className="flex flex-col flex-1 min-w-min gap-5">
+        {/* Top tools bar (delete & edit subnet + create new address + go back) */}
+        <div className="flex flex-row items-center justify-between text-primary-text">
+          <Link to={`/subnets`}>
+            <ChevronsLeft />
+          </Link>
+          <div className="flex flex-row gap-2">
+            <DropdownMenu>
+              <div className="flex flex-col w-full flex-1">
+                <DropdownMenuTrigger asChild className="">
+                  <Button className="gap-1 px-10">
+                    <Plus />
+                    Add Address
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <AddressDialogForm
+                    variant="create"
+                    addressType="reserved"
+                    title={`Reserve Address`}
+                    description={`Reserve Address On Subnet: ${subnetAddress}/${subnet.subnet_cidr}`}
+                    dialogTrigger={
+                      <DropdownMenuItem className="text-address-reserved justify-center" >
+                        Reserve Address
+                      </DropdownMenuItem>
+                    }
+                  />
+                  <DropdownMenuSeparator />
+                  <AddressDialogForm
+                    variant="create"
+                    addressType="dynamic"
+                    title={`Allocate Free Address`}
+                    description={` Allocate Free Address On Subnet: ${subnetAddress}/${subnet.subnet_cidr}`}
+                    dialogTrigger={
+                      <DropdownMenuItem className="text-address-dynamic justify-center">
+                        Allocate Free Address
+                      </DropdownMenuItem>
+                    }
+                  />
+                </DropdownMenuContent>
+              </div>
+            </DropdownMenu>
 
-          <SubnetDialogForm
-            variant="edit"
-            title={`Edit Subnet ${subnetAddress}/${subnet.subnet_cidr}`}
-            dialogTrigger={
-              <Button className="p-3">
-                <Edit />
-              </Button>
-            }
-          />
-          <DeleteConfirmationDialog
-            title={`Delete Subnet ${subnetAddress}/${subnet.subnet_cidr}`}
-            description="This is a permanent action. All Subnets data, address, and details
+            <SubnetDialogForm
+              variant="edit"
+              title={`Edit Subnet ${subnetAddress}/${subnet.subnet_cidr}`}
+              dialogTrigger={
+                <Button className="p-3">
+                  <Edit />
+                </Button>
+              }
+            />
+            <DeleteConfirmationDialog
+              title={`Delete Subnet ${subnetAddress}/${subnet.subnet_cidr}`}
+              description="This is a permanent action. All Subnets data, address, and details
             will be deleted immediately and cannot be recovered."
-            dialogTrigger={
-              <Button className="p-3">
-                <TrashIcon />
-              </Button>
-            }
+              dialogTrigger={
+                <Button className="p-3">
+                  <TrashIcon />
+                </Button>
+              }
+            />
+          </div>
+        </div>
+
+        <Separator />
+
+        {/* Subnet details card */}
+        <Card>
+          <CardContent>
+            <div className="flex flex-row gap-4 justify-around">
+              <ul>
+                <li>
+                  Subnet Address: {subnetAddress}/{subnet.subnet_cidr}
+                </li>
+                <li>Subnet Name: {subnet.name}</li>
+                <li>Subnet Description: {subnet.description}</li>
+              </ul>
+              <Separator orientation="vertical" />
+              <ul>
+                <li> Default Gateway: {subnet.gateway}</li>
+                <li>DNS Servers: {subnet.dns_servers.toString()}</li>
+                <li>
+                  Total Addresses: {Object.keys(subnet.allocated_ips).length}
+                </li>
+              </ul>
+            </div>
+          </CardContent>
+        </Card>
+
+        {/* Address table + search & filter bar */}
+        <div className="flex flex-col gap-4 mt-4">
+          <SearchBar />
+          <DataTable
+            columns={subnetAddressesColumns}
+            data={formatAddressesData(subnet.allocated_ips)}
+            className="h-full w-full"
           />
         </div>
       </div>
+    );
+  }
 
-      <Separator />
-
-      {/* Subnet details card */}
-      <Card>
-        <CardContent>
-          <div className="flex flex-row gap-4 justify-around">
-            <ul>
-              <li>
-                Subnet Address: {subnetAddress}/{subnet.subnet_cidr}
-              </li>
-              <li>Subnet Name: {subnet.name}</li>
-              <li>Subnet Description: {subnet.description}</li>
-            </ul>
-            <Separator orientation="vertical" />
-            <ul>
-              <li> Default Gateway: {subnet.gateway}</li>
-              <li>DNS Servers: {subnet.dns_servers.toString()}</li>
-              <li>
-                Total Addresses: {Object.keys(subnet.allocated_ips).length}
-              </li>
-            </ul>
-          </div>
-        </CardContent>
-      </Card>
-
-      {/* Address table + search & filter bar */}
-      <div className="flex flex-col gap-4 mt-4">
-        <SearchBar />
-        <DataTable
-          columns={subnetAddressesColumns}
-          data={formatAddressesData(subnet.allocated_ips)}
-          className="h-full w-full"
-        />
-      </div>
-    </div>
-  );
+  return toast.error("Unexpected Error Accord", {
+    description: "Looks like the subnet your looking for cannot be found",
+  });
 };
 
 export default SubnetDetailsPage;
